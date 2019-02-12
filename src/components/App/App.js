530a1ecddd9bf4182b0aa76e0511322c99a493e5
Alexey Shevchenko <kamikozz@mail.ru>
2019-02-12 08:17:55 +0300

    added children-routing in the App component

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,45 +1,27 @@
-import React, {Component} from 'react';
-import logo from '../../logo.svg';
+import React from 'react';
 import './App.css';
 import Button from "@material-ui/core/Button";
 import Nav from '../Nav/Nav';
+import List from '../List/List';
+import CaughtPokemons from "../CaughtPokemons/CaughtPokemons";
+import NotFound from "../NotFound/NotFound";
+import Pokemon from "../Pokemon/Pokemon";
+import {Route, Switch} from "react-router-dom";
 
-class App extends Component {
+
+class App extends React.Component {
+
 	render() {
 		return (
 			<div className="App">
-				{/*<Nav />*/}
-				<header className="App-header">
-					<img src={logo} className="App-logo" alt="logo"/>
-					<p>
-						Edit <code>src/App.js</code> and save to reload.
-					</p>
-					<a
-						className="App-link"
-						href="https://reactjs.org"
-						target="_blank"
-						rel="noopener noreferrer"
-					>
-						Learn React
-					</a>
-					<div>
-						<Button variant="contained" color="primary" className={'btn'}>
-							Hello World
-						</Button>
-						<Button variant="contained" color="primary" className={'btn'}>
-							Hello World
-						</Button>
-						<Button variant="contained" color="primary" className={'btn'}>
-							Hello World
-						</Button>
-						<Button variant="contained" color="primary" className={'btn'}>
-							Hello World
-						</Button>
-						<Button variant="contained" color="primary" className={'btn'}>
-							Hello World
-						</Button>
-					</div>
-				</header>
+				<Nav/>
+				<section className="App-section">
+					<Switch>
+						<Route exact path="/" component={List} />
+						<Route path="/pokemons/:id" component={Pokemon} />
+						<Route path="/caught_pokemons/" component={CaughtPokemons} />
+					</Switch>
+				</section>
 			</div>
 		);
 	}
